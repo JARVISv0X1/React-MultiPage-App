b14127a87c2fbd4f2d0fc246a39f13cd271f8dac
Karan Khallar <karan.khallar.courses@gmail.com>
2024-05-31 13:38:55 +0530

    Added Navigation bar in the project

---
+++ src/components/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import MainNavigation from "./MainNavigation";
+
+export default function RootLayout() {
+  return (
+    <>
+      <MainNavigation></MainNavigation>
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </>
+  );
+}
