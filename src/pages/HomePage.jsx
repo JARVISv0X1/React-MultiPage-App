0c00f6ea7bb3f6f52cf2c530163f7b1aad7db1a6
Karan Khallar <karan.khallar.courses@gmail.com>
2024-05-31 13:41:13 +0530

    Added 404 error page in the project for invalid url

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,8 @@
-import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <>
       <h1>My Home Page</h1>
-      <p>
-        go to <Link to={"/products"}>Product Page</Link>
-      </p>
     </>
   );
 }
